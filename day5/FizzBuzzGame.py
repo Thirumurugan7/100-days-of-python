797536947d0fab8cd4d2631f2d3ff1f6665c6486
thirumurugan7 <thiru82003@gmail.com>
2022-10-22 00:24:22 +0530

    day5 over

---
+++ day5/FizzBuzzGame.py
@@ -0,0 +1,11 @@
+#fizzbuzz game
+for i in range(1,101):
+  if((i%3==0) &(i%5==0)):
+    print("FizzBuzz")
+    break
+  elif((i%3==0) & (i%5!=0)):
+    print("Fizz")
+  elif((i%5==0)& (i%3!=0)):
+    print("Buzz")
+  else:
+    print(i)
