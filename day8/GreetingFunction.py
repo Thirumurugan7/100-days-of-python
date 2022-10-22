8f83e54891ffffc9c7dd3c760c1ff3fc283c1926
thirumurugan7 <thiru82003@gmail.com>
2022-10-22 23:41:28 +0530

    geeting function

---
+++ day8/GreetingFunction.py
@@ -0,0 +1,12 @@
+def greet():
+	print("hi")
+	print("hello")
+	print("its thiru")
+
+greet()
+
+def greetWithName(name):
+	print(f"hi {name}")
+	print(f"hello  {name}")
+	print(f"its thiru {name}")
+greetWithName(name="sia")
